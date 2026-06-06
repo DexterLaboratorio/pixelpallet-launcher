9116c19f908d03afa0e4be2dcab376c8170f583c
DexteR <ryanprietsch@gmail.com>
2026-06-06 07:28:38 -0300

    feat: configure PixelPallet distribution URL

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,9 +2,8 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
-// Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+// URL do manifesto de distribuição do PixelPallet (GitHub Pages)
+exports.REMOTE_DISTRO_URL = 'https://dexterlaboratorio.github.io/pixelpallet-distribution/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
